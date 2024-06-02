d3550f6c1a40c179bb0d23f254ae96de8aeeafca
Lucas Tanino <lucastanino@live.com>
2024-06-02 15:11:11 -0300

    estilizando a sidedebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/WisterianWell.png" />
-    <Titulo fontSize={20}>Lucas Tanino</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Lucas Fujii</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        WisterianWell
+      </Paragrafo>
+      <Descricao fontSize={12} tipo="principal">
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
